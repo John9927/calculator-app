ca2e560cef84b3ade637eb11ef3eb7ed5212a04d
Giovanni Izzi <giovanni9972@libero.it>
2021-05-24 11:03:49 +0200

    Fix style

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -6,43 +6,29 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent implements OnInit, AfterViewInit {
+export class DisplayComponent implements OnInit {
   @ViewChild('result') result: any;
   @ViewChild('numbers') numbers: any;
 
   number: any;
   results: any;
+  variableNumber = false;
+  variableResult = false;
   constructor(public calculatedService: CalculatedService) { }
-
-  ngAfterViewInit() {
-    if (this.calculatedService.storedTheme == "theme-one") {
-      this.numbers.nativeElement.setAttribute("style", "background: var(--screen-background-1)")
-      this.result.nativeElement.setAttribute("style", "background: var(--screen-background-1)")
-    } else if (this.calculatedService.storedTheme == "theme-two") {
-      this.numbers.nativeElement.setAttribute("style", "background: var(--screen-background-2)")
-      this.result.nativeElement.setAttribute("style", "background: var(--screen-background-2)")
-    } else if (this.calculatedService.storedTheme == "theme-three") {
-      this.numbers.nativeElement.setAttribute("style", "background: var(--screen-background-2)")
-      this.result.nativeElement.setAttribute("style", "background: var(--screen-background-2)")
-    }
-  }
 
   ngOnInit(): void {
     this.calculatedService.state$.subscribe(res => {
       this.number = res.numbers[0] + res.operation + res.numbers[1];
       this.results = res.result;
-    });
 
-    setInterval(() => {
-      if (this.results == null) {
-        document.getElementById('container').style.display = "flex";
-        document.getElementById('display').style.display = "none";
+      if (this.number) {
+        this.variableNumber = true;
+        this.variableResult = false;
+      } else {
+        this.variableNumber = false;
+        this.variableResult = true;
       }
-      else {
-        document.getElementById('container').style.display = "none";
-        document.getElementById('display').style.display = "flex";
-      }
-    }, 10);
+    });
   }
 
 }
